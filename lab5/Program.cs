c01ce0f6c35ee3bc61060981ab1887e470e669f7
ryosuke <killua70ld1k@gmail.com>
2024-05-25 21:06:19 +0300

    Добавьте файлы проекта.

---
+++ lab5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой консольный калькулятор");
+
+        Console.Write("Введите первое число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите оператор (+, -, *, /): ");
+        char op = Console.ReadLine()[0];
+
+        Console.Write("Введите второе число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Неверный оператор");
+                return;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
+
